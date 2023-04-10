48d15b5a29b1a39e011e7c6556d0ec3b72e05845
reza72ahmadi <reza72.ahmadi@gmail.com>
2023-04-09 20:09:05 -0700

    project added

---
+++ src/components/banner.js
@@ -0,0 +1,8 @@
+const Banner = () => {
+    return (
+        <div className="banner">
+            <h1>This is banner section</h1>
+        </div>
+    )
+}
+export default Banner;
---
+++ src/components/header.js
@@ -0,0 +1,14 @@
+const Header = () =>{
+
+    return (
+        <div className="header">
+        <img src="../logo192.png" className="logo"/>
+            <ul>
+                <li>Home</li>
+                <li>About</li>
+                <li>Contact</li>
+            </ul>
+        </div>
+    )
+}
+export default Header;
